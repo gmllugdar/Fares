8ea34fb897d37ad3779f3b414501c0b79d13c26c
guillo93 <gmllugdar@gmail.com>
2022-09-27 18:13:16 -0300

    finish topic 3

--- 03-MVC-Laravel/laravel/resources/views/peliculas/edit.blade.php
+++ 03-MVC-Laravel/laravel/resources/views/peliculas/edit.blade.php
@@ -16,10 +16,15 @@
 
 		<label for="sinopsis">sinopsis</label>
 		<input name="sinopsis" id="sinopsis" type="text" placeholder="sinopsis" value="{{ $pelicula->sinopsis }}">
-
+		<br>
 		<label for="imagen">imagen</label>
-		<input name="imagen" id="imagen" type="file" accept="image/png, image/gif, image/jpeg">
 
+		<input name="imagen" id="imagen" type="file" accept="image/png, image/gif, image/jpeg">
+		<div id="drop" ondrop="dropHandler(event)" ondragover="event.preventDefault()">
+			<img id="image" src="" alt="img">
+			
+		</div>
+		<br>
 		<label for="ActorPrincipalID">ActorPrincipalID</label>
 		<select name="ActorPrincipalID" id="ActorPrincipalID">
 			@foreach ($actores as $actor)
@@ -29,5 +34,31 @@
 
 		<button type="submit">Guardar</button>
 	</form>
+<style>
+	#drop{ width:200px; border: 3px dashed red ; height: 200px; }
+</style>
+<script>
+	 let image = document.getElementById('image');
+	 let imageinput = document.getElementById('imagen');
+	 function dropHandler(ev) {
+  
+  ev.preventDefault();
+  
 
+	let file = ev.dataTransfer.items[0].getAsFile()
+	console.log("datos:  ",file.name)
+	const readfile = new FileReader()
+	readfile.readAsDataURL(file)
+	readfile.onloadend= function(){
+		image.src= this.result;
+		
+	}
+	const dt = new DataTransfer()
+	dt.items.add(file)
+	imageinput.files=dt.files
+	
+	
+		
+}
+</script>
 @endsection
